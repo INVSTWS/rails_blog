f304a47da8947b0d2a8acbc9d62d7f560da7817a
INVSTWS <bizzare9977@gmail.com>
2024-03-18 18:02:38 +0200

    user avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
     before_action :set_query
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     def set_query
         @query = Post.ransack(params[:q])
     end 
+
+
+
+    
+
+    protected
+  
+    def configure_permitted_parameters
+      attributes = [:username, :email, :password, :password_confirmation]
+      devise_parameter_sanitizer.permit(:sign_in, keys: attributes)
+      devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+      devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+    end
 end
